a667008f2b0a3cb5205de9fd7eda6a11b98de595
Alvaro Paez <paezguerraalvaro@gmail.com>
2026-02-06 13:57:18 +0100

    feat(test): compile doctest with DOCTEST_CONFIG_SUPER_FAST_ASSERTS

--- third_party/doctest/cmake/doctest-target.cmake
+++ third_party/doctest/cmake/doctest-target.cmake
@@ -9,4 +9,7 @@ target_include_directories(doctest
 add_library(doctest_with_main INTERFACE)
 add_library(doctest::doctest_with_main ALIAS doctest_with_main)
 target_link_libraries(doctest_with_main INTERFACE doctest::doctest)
-target_compile_definitions(doctest_with_main INTERFACE DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN)
+target_compile_definitions(doctest_with_main 
+    INTERFACE 
+        DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN 
+        DOCTEST_CONFIG_SUPER_FAST_ASSERTS)
